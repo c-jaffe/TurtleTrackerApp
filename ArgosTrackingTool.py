7512e1fa431ba41a1156a64679fd7788c53fe468
c-jaffe <catherine.jaffe@duke.edu>
2020-09-20 14:32:34 -0400

    Intial commit of ARGOS script

---
+++ ArgosTrackingTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Cate Jaffe (cmj64@duke.edu)
+# Date:   September 20 2020
+#-------------------------------------------------------------
