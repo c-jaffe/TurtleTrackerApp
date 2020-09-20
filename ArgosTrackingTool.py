f3ae14389ee00c11463299247a27a2b30fef621f
c-jaffe <catherine.jaffe@duke.edu>
2020-09-20 15:20:56 -0400

    reads data in from argos file

--- ArgosTrackingTool.py
+++ ArgosTrackingTool.py
@@ -9,8 +9,22 @@
 # Date:   September 20 2020
 #-------------------------------------------------------------
 
+# Create variable pointing to data file.
+file_name = './data/raw/sara.txt'
+
+# create file object from file
+file_object = open(file_name, 'r')
+
+# read contents of file to list.
+line_list = file_object.readlines()
+
+# close file
+file_object.close()
+
+
+
 # Pull one line of data from file
-lineString = "21892	29051	7/3/2003 9:23	B	0	33.887	-77.95	43.983	-128.418	2	0	-132	96	1	401 651133.1	0"
+lineString = line_list[87]
 
 # Split string into list of items
 lineData = lineString.split()
@@ -22,3 +36,4 @@ obs_lat = lineData[6]
 obs_lon = lineData[7]
 
 print( f" Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
+
